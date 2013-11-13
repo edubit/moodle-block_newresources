33e37970174d36177d3c2b7e90ed2a3f076972a1
André Yamin <andreyamin@gmail.com>
2013-11-12 22:50:34 -0200

    Criando a estrutura do bloco

---
+++ block_newresources.php
@@ -0,0 +1,7 @@
+<?php
+class block_newresources extends block_base {
+    public function init() {
+        $this->title = get_string('newresources', 'block_newresources');
+    }
+    // The PHP tag and the curly bracket for the class definition 
+    // will only be closed after there is another function added in the next section.
---
+++ lang/en/block_newresources.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'New Resources Block';
+$string['simplehtml'] = 'New R';
+$string['simplehtml:addinstance'] = 'Add a new New Resources Block';
+$string['simplehtml:myaddinstance'] = 'Add a new New Resrources Block to the My Moodle page';
