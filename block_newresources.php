4e634ae1d73e2c7534c7b4c0b805add3daf9a08e
Helbert dos Santos <helbert.ds@gmail.com>
2013-11-25 22:08:42 -0200

    Bloco instalado e operando no Moodle 2.6

--- block_newresources.php
+++ block_newresources.php
@@ -3,5 +3,15 @@ class block_newresources extends block_base {
     public function init() {
         $this->title = get_string('newresources', 'block_newresources');
     }
-    // The PHP tag and the curly bracket for the class definition 
-    // will only be closed after there is another function added in the next section.
+    public function get_content() {
+    if ($this->content !== null) {
+      return $this->content;
+    }
+ 
+    $this->content         =  new stdClass;
+    $this->content->text   = 'The content of our SimpleHTML block!';
+    $this->content->footer = 'Footer here...';
+ 
+    return $this->content;
+  }
+}   // Here's the closing bracket for the class definition
--- lang/en/block_newresources.php
+++ lang/en/block_newresources.php
@@ -1,5 +1,5 @@
 <?php
 $string['pluginname'] = 'New Resources Block';
-$string['simplehtml'] = 'New R';
-$string['simplehtml:addinstance'] = 'Add a new New Resources Block';
-$string['simplehtml:myaddinstance'] = 'Add a new New Resrources Block to the My Moodle page';
+$string['newresources'] = 'New Resources';
+$string['newresources:addinstance'] = 'Add a new New Resources Block';
+$string['newresources:myaddinstance'] = 'Add a new New Resrources Block to the My Moodle page';
