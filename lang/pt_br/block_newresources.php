1a1e5c93f1a16e65e64f3e870f5589f3029e4c22
Helbert dos Santos <helbert.ds@gmail.com>
2013-11-29 00:24:28 -0200

    Internacionalizadao do bloco, language pack PT_BR

---
+++ lang/pt_br/block_newresources.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Strings for component 'block_newresources', language 'en'
+ *
+ * @package   block_newresources
+ * @copyright 2013 Edubit.com.br
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Bloco Novos Recursos';
+$string['newresources'] = 'Novos Recursos';
+$string['advsearch'] = 'Busca Avançada';
+$string['newresources:addinstance'] = 'Incluir o bloco Novos Recursos';
+$string['newresources:myaddinstance'] = 'Incluir o bloco Novos Recursos no My Moodle';
+
+//CONFIGS
+$string['headerconfig'] = 'Config Header';
+$string['descconfig'] = '';
+$string['labelnotify'] = 'Notificação';
+$string['descnotify'] = '';
+$string['labelfreqnotify'] = 'Frequencia de Notificações';
+$string['descfreqnotify'] = '';
+$string['labeltimenotify'] = 'Horário de Notificação';
+$string['desctimenotify'] = '';
+$string['descadvsearch'] = ''; 
+$string['labelinterval'] = 'Intervalo';
+$string['desinterval'] = 'Intervalo em dias padrão para o campo DATA de INICIO no formulário de Busca Avançada';
+$string['labelitemsperpage'] = 'Resultados por página';
+$string['desitemsperpage'] = 'Quantidade de Resultados por página na Busca Avançada';
+
+//ADVANCED SEARCH
+$string['search'] = 'Pesquisar';
+$string['dateend'] = 'Data final';
+$string['titlecourse'] = 'Disciplina'; 
+$string['titleordescription'] = 'Título ou Descrição do Módulo';
+$string['datestart'] = 'Data de início';
+
+$string['dateadded'] = 'Data de Publicação';
