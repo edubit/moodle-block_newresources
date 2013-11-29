057d2e2040d3164df9a1bccc8a910e00463a14be
Helbert dos Santos <helbert.ds@gmail.com>
2013-11-29 00:15:31 -0200

    Language pack EN_us

--- lang/en/block_newresources.php
+++ lang/en/block_newresources.php
@@ -1,5 +1,38 @@
 <?php
+/**
+ * Strings for component 'block_newresources', language 'en'
+ *
+ * @package   block_newresources
+ * @copyright 2013 Edubit.com.br
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['pluginname'] = 'New Resources Block';
 $string['newresources'] = 'New Resources';
+$string['advsearch'] = 'Advanced Search';
 $string['newresources:addinstance'] = 'Add a new New Resources Block';
 $string['newresources:myaddinstance'] = 'Add a new New Resrources Block to the My Moodle page';
+
+//CONFIGS
+$string['headerconfig'] = 'Config Header';
+$string['descconfig'] = '';
+$string['labelnotify'] = 'Nofify';
+$string['descnotify'] = '';
+$string['labelfreqnotify'] = 'Notifications`s Frequency';
+$string['descfreqnotify'] = '';
+$string['labeltimenotify'] = 'Time of Notify';
+$string['desctimenotify'] = '';
+$string['descadvsearch'] = ''; 
+$string['labelinterval'] = 'Interval';
+$string['desinterval'] = '';
+$string['labelitemsperpage'] = 'Items per page';
+$string['desitemsperpage'] = '';
+
+//ADVANCED SEARCH
+$string['search'] = 'Search';
+$string['dateend'] = 'End Date';
+$string['titlecourse'] = 'Course'; 
+$string['titleordescription'] = 'Title or Description of Module';
+$string['datestart'] = 'Start Date';
+
+$string['dateadded'] = 'Added Date';
